c504ed1719ca1f11d473e7b92e5fc47a8a3ab4b8
aabbcc5050 <aabbcc5050@qq.com>
2017-11-02 23:33:56 +0800

    000

---
+++ MyJava2/src/com/my/test/RunableTest.java
@@ -0,0 +1,27 @@
+package com.my.test;
+
+import java.util.Map;
+
+public class RunableTest implements Runnable{
+	
+	private Map<String, Object> params;
+	public Map<String, Object> getParams() {
+		return params;
+	}
+	public void setParams(Map<String, Object> params) {
+		this.params = params;
+	}
+
+
+	@Override
+	public void run() {
+		try {
+			System.out.println(getParams());
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
---
+++ MyJava2/src/com/my/test/Test02.java
@@ -0,0 +1,38 @@
+package com.my.test;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Test02 {
+
+	private final static int THREAD_POOL_SIZE=10;
+	private final static int index=0;
+	
+	public static void main(String[] args) {
+		List<Map<String, Object>> list=getList();
+		ExecutorService  service=Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+		while(list!=null&&!list.isEmpty()){
+			Map<String, Object> params=list.get(index);
+			list.remove(0);
+			RunableTest task=new RunableTest();
+			task.setParams(params);
+			service.execute(task);
+		}
+		service.shutdown();
+	}
+	
+	public static List<Map<String, Object>> getList(){
+		List<Map<String, Object>> result=new ArrayList<>();
+		for (int i = 0; i < 100; i++) {
+			Map<String, Object> map=new HashMap<>();
+			map.put("k"+i, "v"+i);
+			result.add(map);
+		}
+		return result;
+	}
+}
